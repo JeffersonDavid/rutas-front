8417b72a21af74d69af50075acf4278c2b171ec2
JeffersonDavid <jeffersondvid@hotmail.com>
2024-05-06 22:06:21 +0200

    add auth

--- src/app/components/auth/dataCript.tsx
+++ src/app/components/auth/dataCript.tsx
@@ -1,44 +1,27 @@
-import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
 import { UserData } from '../../appContexts/AuthContext';
 
-export const rest_authentication = async (userData: UserData): Promise<void> => {
-    try {
-      // Cifra los datos del usuario
-      const encryptedData = crypt(userData);
-      console.log('datos encriptados '+ encryptedData )
-  
-      // Realiza una solicitud HTTP POST al backend para autenticar al usuario
-      const response = await fetch('url_del_backend', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ encryptedData }) // Envía los datos cifrados en el cuerpo de la solicitud
-      });
-  
-      if (response.ok) {
-        // La solicitud fue exitosa, puedes manejar la respuesta aquí si es necesario
-        console.log('Autenticación exitosa');
-      } else {
-        // La solicitud falló, maneja el error aquí si es necesario
-        console.error('Error en la autenticación');
-      }
-    } catch (error) {
-      console.error('Error al realizar la solicitud:', error);
-    }
-  };
-  
+export const rest_authentication = async ( userData: UserData ): Promise<void> => {
 
-export const crypt = (data: any): string => {
-    const jsonString = JSON.stringify(data);
-    const key = generateRandomKey();
-    const iv = crypto.randomBytes(16); // Initialization vector (IV) de 16 bytes para AES
-    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-    let encrypted = cipher.update(jsonString, 'utf-8', 'hex');
-    encrypted += cipher.final('hex');
-    return encrypted;
-  };
+   const data_crypted = crypt (userData );
 
-const generateRandomKey = (): Buffer => {
-    return crypto.randomBytes(32); // 32 bytes = 256 bits
-  };
+   console.log('data crypted')
+   console.log(data_crypted)
+   
+ 
+};
+
+
+
+const crypt = (payload: JsonObject): string => {
+    const secretKey = 'test';
+    // Define el tiempo de expiración del token (puedes ajustarla según sea necesario)
+    const expirationTime = '1h'; // 1 hora de duración del token
+    // Genera el token JWT
+    const token = jwt.sign(payload, secretKey, { expiresIn: expirationTime });
+    return token
+};
+
+interface JsonObject {
+    [key: string]: any;
+}
