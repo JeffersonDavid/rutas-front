6a2ff7ff6582005d947c4fcf749125cf8702ab0d
jc <jeffersondvid@hotmail.com>
2024-06-07 11:22:03 +0000

    add private room

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,14 @@
 
 import Middleware from "../components/auth/pageMiddleware";
-import UserConnection from "../components/sockets/users/setUserState";
 import UsersTable from "../components/sockets/users/usertTable";
 
 const Dashboard = () => {
   
   return (
     <>
-      <Middleware>
           <div className="mt-[10vh]">
-            <UsersTable></UsersTable>
-           </div>
-      </Middleware>
+              <UsersTable></UsersTable>
+          </div>
     </>
   );
 };
--- src/app/quick-play/page.tsx
+++ src/app/quick-play/page.tsx
@@ -1,13 +1,32 @@
-'use client'
-import React, { useState, useEffect, useCallback } from 'react';
-import { useAuth } from '../appContexts/AuthContext';
-import CustomAlert from '../components/alerts/customAlert';
-import { useRouter } from 'next/navigation';
-import { useLoader } from '../appContexts/AppLoader';
+'use client';
+import { useState, useEffect } from 'react';
+import { useWebSocket } from '../appContexts/WebsocketContext';
+import { useAuth } from '@/app/appContexts/AuthContext';
 
 const QuickPlay: React.FC = () => {
-  const { login } = useAuth();
- return ('quick play');
+
+  const [searching, setSearching] = useState(false);
+
+  const handleSearchClick = () => {
+    setSearching(true);
+    // Aquí puedes agregar lógica adicional para manejar la búsqueda de partida, por ejemplo, emitir un evento WebSocket.
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-gray-900">
+      <button
+        className="bg-gray-800 text-white p-4 rounded mb-4 hover:bg-gray-700 transition duration-300"
+        onClick={handleSearchClick}
+      >
+        Buscar partida
+      </button>
+      {searching && (
+        <div className="text-center text-white">
+          <p className="bg-gray-800 p-2 rounded animate-pulse">Buscando...</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default QuickPlay;
