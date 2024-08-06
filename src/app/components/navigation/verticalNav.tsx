1adcd436b55c08590321ffb3afb9c7126c71b389
JeffersonDavid <jeffersondvid@hotmail.com>
2024-08-06 14:13:17 +0000

    add solid login

--- src/app/components/navigation/verticalNav.tsx
+++ src/app/components/navigation/verticalNav.tsx
@@ -9,19 +9,19 @@ interface VerticalNavComponentProps {
 }
 
 const VerticalNavComponent: React.FC<VerticalNavComponentProps> = ({ isCollapsed, toggleMenu }) => {
-  const { user_is_logged } = useAuth();
+  const { user_is_logged, logout } = useAuth();
 
   return (
     <nav className={`bg-gray-900 ${isCollapsed ? 'w-20' : 'w-64'} h-screen py-4 px-2 flex flex-col justify-between transition-all duration-300 border-r border-gray-700`}>
-      {user_is_logged ? (
-        <>
-          <div>
-            <div className='flex justify-between items-center px-2'>
-              <span className={`text-white text-2xl font-semibold ${isCollapsed ? 'hidden' : 'block'}`}>Xchess</span>
-              <button onClick={toggleMenu} className="text-white">
-                <FaBars />
-              </button>
-            </div>
+      <div>
+        <div className='flex justify-between items-center px-2'>
+          <span className={`text-white text-2xl font-semibold ${isCollapsed ? 'hidden' : 'block'}`}>Xchess</span>
+          <button onClick={toggleMenu} className="text-white">
+            <FaBars />
+          </button>
+        </div>
+        {user_is_logged && (
+          <>
             <ul className="mt-4 space-y-2">
               <li className="flex items-center px-2 py-2 text-gray-300 hover:bg-gray-700 rounded-md cursor-pointer">
                 <FaTh className="text-xl" />
@@ -33,19 +33,22 @@ const VerticalNavComponent: React.FC<VerticalNavComponentProps> = ({ isCollapsed
                 <span className={`ml-2 text-lg ${isCollapsed ? 'hidden' : 'block'}`}><a href='/quick-play'> Partida rápida </a></span>
               </li>
             </ul>
-          </div>
-          <div className="px-2 py-4">
-            <ul className="space-y-2">
-              <li className="flex items-center px-2 py-2 text-gray-300 hover:bg-gray-700 rounded-md cursor-pointer">
-                <FaChess className="text-xl" />
-                <span className={`ml-2 text-lg ${isCollapsed ? 'hidden' : 'block'}`}>Logout</span>
-              </li>
-            </ul>
-          </div>
-        </>
-      ) : (
-        <div className="text-center text-white py-4">www.test.com</div>
-      )}
+            <div className="mt-6 text-xs text-gray-400 uppercase px-2">Account</div>
+          </>
+        )}
+      </div>
+      <div className="px-2 py-4">
+        {user_is_logged ? (
+          <ul className="space-y-2">
+            <li className="flex items-center px-2 py-2 text-gray-300 hover:bg-gray-700 rounded-md cursor-pointer" onClick={logout}>
+              <FaChess className="text-xl" />
+              <span className={`ml-2 text-lg ${isCollapsed ? 'hidden' : 'block'}`}>Logout</span>
+            </li>
+          </ul>
+        ) : (
+          <div className="text-center text-white py-4">www.test.com</div>
+        )}
+      </div>
     </nav>
   );
 };
